eaa514a04fbff7a1df6a9df3ae0bcc5aac027d4b
higorasss <higorass@gmail.com>
2022-09-13 19:28:37 +0000

    Testei a integração entre bot e Discord

---
+++ chatbot.py
@@ -0,0 +1,32 @@
+from os import getenv
+from dotenv import load_dotenv
+import discord
+load_dotenv()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+        if message.content.startswith('!Bom dia'):
+            await message.reply('Para quem?', mention_author=True)
+        
+        if message.content.startswith('!O Brasil vai ganhar a copa?'):
+            await message.reply('Confia', mention_author=True)
+
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(getenv("DISCORD_TOKEN"))
